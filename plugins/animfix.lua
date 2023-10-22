36914a6223cc25372b09f8d032e434da7094b26a
Naidru <joeypatten8@gmail.com>
2023-10-22 17:10:24 -0300

    Fix Animations

---
+++ plugins/animfix.lua
@@ -0,0 +1,19 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Animation Fixes for Various Addons"
+PLUGIN.author = "Naidru"
+PLUGIN.description = "Improves compatability for a bunch of models."
+
+ix.anim.SetModelClass("models/cfe_pm/cfe_male_pm/cfe_male_01_pm.mdl", "metrocop")
+ix.anim.SetModelClass("models/cfe_pm/cfe_female_pm/cfe_female_01_pm.mdl", "metrocop")
+ix.anim.SetModelClass("models/conceptbine_policeforce/conceptpolice_wst.mdl", "metrocop")
+ix.anim.SetModelClass("models/conceptbine_policeforce/conceptpolice_nemez.mdl", "metrocop")
+ix.anim.SetModelClass("models/cmb_synth_elite.mdl", "metrocop")
+ix.anim.SetModelClass("models/cmb_synth_soldier.mdl", "metrocop")
+ix.anim.SetModelClass("models/betametro/betametro.mdl", "metrocop")
+ix.anim.SetModelClass("models/tnb/combine/metrocop_female.mdl", "metrocop")
+ix.anim.SetModelClass("models/tnb/combine/metrocop.mdl", "metrocop")
+ix.anim.SetModelClass("models/elitepoliceresistance.mdl", "metrocop")
+ix.anim.SetModelClass("models/combinesoldier2002.mdl", "metrocop")
+ix.anim.SetModelClass("models/combine_soldierresistancenormalshotgun.mdl", "metrocop")
+ix.anim.SetModelClass("models/betasoldiercombine.mdl", "metrocop")
+ix.anim.SetModelClass("models/combine_soldier_resistancenormal.mdl", "metrocop")
